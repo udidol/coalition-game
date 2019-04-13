73fc5b4e49ce5d686c574533b3dc3c14c844721e
udidol <udi.dollberg@gmail.com>
2019-04-13 18:42:55 +0300

    added url parameters support

--- src/App.js
+++ src/App.js
@@ -14,10 +14,10 @@ class App extends Component {
 
     this.state = {
       parties: {
-        'likud': { 'name': 'likud', 'hebrewName': 'הליכוד', 'numOfMandates': 35, 'color': this.defaultColor, 'clicked': false },
+        'likud': { 'name': 'likud', 'hebrewName': 'הליכוד', 'numOfMandates': 36, 'color': this.defaultColor, 'clicked': false },
         'kacholLavan': { 'name': 'kacholLavan', 'hebrewName': 'כחול-לבן', 'numOfMandates': 35, 'color': this.defaultColor, 'clicked': false },
-        'yahadutHatora': { 'name': 'yahadutHatora', 'hebrewName': 'יהדות התורה', 'numOfMandates': 8, 'color': this.defaultColor, 'clicked': false },
         'shas': { 'name': 'shas', 'hebrewName': 'ש"ס', 'numOfMandates': 8, 'color': this.defaultColor, 'clicked': false },
+        'yahadutHatora': { 'name': 'yahadutHatora', 'hebrewName': 'יהדות התורה', 'numOfMandates': 7, 'color': this.defaultColor, 'clicked': false },
         'haAvoda': { 'name': 'haAvoda', 'hebrewName': 'העבודה', 'numOfMandates': 6, 'color': this.defaultColor, 'clicked': false },
         'hadashTaal': { 'name': 'hadashTaal', 'hebrewName': 'חד"ש-תע"ל', 'numOfMandates': 6, 'color': this.defaultColor, 'clicked': false },
         'ichudYamin': { 'name': 'ichudYamin', 'hebrewName': 'איחוד מפלגות הימין', 'numOfMandates': 5, 'color': this.defaultColor, 'clicked': false },
@@ -29,19 +29,7 @@ class App extends Component {
       coalitionMandates: 0,
       shamelessClicked: false,
       coalition: [],
-      // coalition: {
-      //   'likud': false,
-      //   'kacholLavan': false,
-      //   'yahadutHatora': false,
-      //   'shas': false,
-      //   'haAvoda': false,
-      //   'hadashTaal': false,
-      //   'ichudYamin': false,
-      //   'israelBeitenu': false,
-      //   'meretz': false,
-      //   'kulanu': false,
-      //   'raamBalad': false
-      // },
+      urlParams: window.location.search.substr(11).split('+'),
       description: 'הרכיבו קואליציה לפי תוצאות בחירות 2019! שתפו עם חבריכם!'
     }
 
@@ -49,14 +37,26 @@ class App extends Component {
     this.toggleShameless = this.toggleShameless.bind(this);
   }
 
+  componentDidMount() {
+    let initialStateCopy = Object.assign({}, this.state);
+
+    for (let party in initialStateCopy.parties) {
+      if (this.state.urlParams !== '' && this.state.urlParams.indexOf(party) !== -1) {
+        initialStateCopy.parties[party].clicked = true;
+        initialStateCopy.coalitionMandates += initialStateCopy.parties[party].numOfMandates;
+        initialStateCopy.coalition.push(initialStateCopy.parties[party].hebrewName);
+      }
+    }
+
+    initialStateCopy.description = `הקואליציה שלי: ${initialStateCopy.coalition.join(', ')}`;
+
+    this.setState(initialStateCopy);
+  }
+
   updateMetaDescription() {
     // Update og description in state
     let tempDescription = this.state.coalition.join(', ');
-    this.setState({ description: `הקואליציה שלי: ${tempDescription}` }, this.logDescription);
-  }
-
-  logDescription() {
-    console.log(this.state.description);
+    this.setState({ description: `הקואליציה שלי: ${tempDescription}` });
   }
 
   onButtonClicked(event) {
@@ -66,15 +66,18 @@ class App extends Component {
     let partyToUpdate = event.currentTarget.name;
 
     // create copy of the state coalition array
-    let coalitionArr = Array.from(this.state.coalition);
-    let partyIndex = coalitionArr.indexOf(this.state.parties[partyToUpdate].hebrewName);
+    let hebrewCoalitionArr = Array.from(this.state.coalition);
+    let newUrlParams = Array.from(this.state.urlParams);
+    let partyKeyIndex = newUrlParams.indexOf(partyToUpdate);
+    let hebrewPartyIndex = hebrewCoalitionArr.indexOf(this.state.parties[partyToUpdate].hebrewName);
 
     if (this.state.parties[partyToUpdate].clicked) {
 
-      // check if party exists in the coalition array in state, if it does, remove it from the coalition array
-      if (partyIndex !== -1) {
-        coalitionArr.splice(partyIndex, 1);
-        this.setState({coalition: coalitionArr}, this.updateMetaDescription);
+      // check if party hebrew name exists in the coalition array in state, if it does, remove it from the coalition array
+      if (hebrewPartyIndex !== -1) {
+        hebrewCoalitionArr.splice(hebrewPartyIndex, 1);
+        newUrlParams.splice(partyKeyIndex, 1);
+        this.setState({coalition: hebrewCoalitionArr, urlParams: newUrlParams}, this.updateMetaDescription);
       }
 
       // Update number of coalition mandates in state
@@ -82,10 +85,11 @@ class App extends Component {
     }
 
     else if (!this.state.parties[partyToUpdate].clicked) {
-      // check if party exists in the coalition array in state, if it doesn't, add it to the coalition array
-      if (partyIndex === -1) {
-        coalitionArr.push(this.state.parties[partyToUpdate].hebrewName);
-        this.setState({coalition: coalitionArr}, this.updateMetaDescription);
+      // check if party hebrew name exists in the coalition array in state, if it doesn't, add it to the coalition array
+      if (hebrewPartyIndex === -1) {
+        hebrewCoalitionArr.push(this.state.parties[partyToUpdate].hebrewName);
+        newUrlParams.push(partyToUpdate);
+        this.setState({coalition: hebrewCoalitionArr, urlParams: newUrlParams}, this.updateMetaDescription);
       }
 
       // Update number of coalition mandates in state
@@ -109,12 +113,12 @@ class App extends Component {
 
   render() {
     const parties = this.state.parties;
-
+    
     return (
       <div className="wrapper">
         <MetaTags>
           <meta name="description" content={`${this.state.coalitionMandates >= 61 ? this.state.description : 'הרכיבו קואליציה לפי תוצאות בחירות 2019! שתפו עם חבריכם!'}`} />
-          <meta property="og:description" content={this.state.description} />
+          <meta property="og:description" content={`${this.state.coalitionMandates >= 61 ? this.state.description : 'הרכיבו קואליציה לפי תוצאות בחירות 2019! שתפו עם חבריכם!'}`} />
         </MetaTags>
         <div className="App">
           <div className="app-title">
@@ -130,9 +134,9 @@ class App extends Component {
               <div className="coalitionSuccess">מזל טוב! יצרת קואליציה!</div>
               <div>
                 <div className="rtl shareTitle center">שתפו:</div>
-                <Facebook link="https://udidollberg.com/coalitiomat2019" />
-                <Twitter link="https://udidollberg.com/coalitiomat2019" />
-                <Whatsapp link="https://udidollberg.com/coalitiomat2019" />
+                <Facebook link={`https://udidollberg.com/coalitiomat2019${this.state.urlParams !== "" ? `?coalition=${this.state.urlParams.join('+')}` : ''}`} />
+                <Twitter link={`https://udidollberg.com/coalitiomat2019${this.state.urlParams !== "" ? `?coalition=${this.state.urlParams.join('+')}` : ''}`} />
+                <Whatsapp link={`https://udidollberg.com/coalitiomat2019${this.state.urlParams !== "" ? `?coalition=${this.state.urlParams.join('+')}` : ''}`} />
               </div>
             </div>
           </div>
@@ -141,8 +145,8 @@ class App extends Component {
             <div className="center hamiflagot" style={{fontSize: '28px', marginBottom: '20px', fontWeight: 'bold'}}>המפלגות</div>
             <Party partyName={parties.likud.name} partyNameHebrew={parties.likud.hebrewName} mandates={parties.likud.numOfMandates} color={parties.likud.color} isClicked={parties.likud.clicked} onClicked={this.onButtonClicked} />
             <Party partyName={parties.kacholLavan.name} partyNameHebrew={parties.kacholLavan.hebrewName} mandates={parties.kacholLavan.numOfMandates} color={parties.kacholLavan.color} isClicked={parties.kacholLavan.clicked} onClicked={this.onButtonClicked} />
-            <Party partyName={parties.yahadutHatora.name} partyNameHebrew={parties.yahadutHatora.hebrewName} mandates={parties.yahadutHatora.numOfMandates} color={parties.yahadutHatora.color} isClicked={parties.yahadutHatora.clicked} onClicked={this.onButtonClicked} />
             <Party partyName={parties.shas.name} partyNameHebrew={parties.shas.hebrewName} mandates={parties.shas.numOfMandates} color={parties.shas.color} isClicked={parties.shas.clicked} onClicked={this.onButtonClicked} />
+            <Party partyName={parties.yahadutHatora.name} partyNameHebrew={parties.yahadutHatora.hebrewName} mandates={parties.yahadutHatora.numOfMandates} color={parties.yahadutHatora.color} isClicked={parties.yahadutHatora.clicked} onClicked={this.onButtonClicked} />
             <Party partyName={parties.haAvoda.name} partyNameHebrew={parties.haAvoda.hebrewName} mandates={parties.haAvoda.numOfMandates} color={parties.haAvoda.color} isClicked={parties.haAvoda.clicked} onClicked={this.onButtonClicked} />
             <Party partyName={parties.hadashTaal.name} partyNameHebrew={parties.hadashTaal.hebrewName} mandates={parties.hadashTaal.numOfMandates} color={parties.hadashTaal.color} isClicked={parties.hadashTaal.clicked} onClicked={this.onButtonClicked} />
             <Party partyName={parties.ichudYamin.name} partyNameHebrew={parties.ichudYamin.hebrewName} mandates={parties.ichudYamin.numOfMandates} color={parties.ichudYamin.color} isClicked={parties.ichudYamin.clicked} onClicked={this.onButtonClicked} />
